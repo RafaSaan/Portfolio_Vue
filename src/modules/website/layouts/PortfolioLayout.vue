da35ecf80f8fa0a2ae51eb83883fefe56b5920ff
rafa <rafasaanchez@icloud.com>
2021-11-28 13:19:03 -0600

    add sections

--- src/modules/website/layouts/PortfolioLayout.vue
+++ src/modules/website/layouts/PortfolioLayout.vue
@@ -1,16 +1,22 @@
 <template>
   <Home />
   <SliderTech />
+  <PreviewProjects />
+  <Footer />
 </template>
 
 <script>
 import Home from "../components/Home";
 import SliderTech from "../components/SliderTech";
+import PreviewProjects from "../components/PreviewProjects";
+import Footer from "../../../components/Footer";
 export default {
   name: "portfolioLayout",
   components: {
     Home,
     SliderTech,
+    PreviewProjects,
+    Footer,
   },
 };
 </script>

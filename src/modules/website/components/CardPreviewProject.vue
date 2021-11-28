f112761f1e4a9d2a7a2dd778a6e9518cf23bc912
rafa <rafasaanchez@icloud.com>
2021-11-28 13:19:26 -0600

    add section preview projects

---
+++ src/modules/website/components/CardPreviewProject.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="cardPreview">
+    <slot name="img" />
+    <div class="card__info">
+      <h3>{{ titleCard }}</h3>
+      <div class="card__type">
+        <div class="card__date">{{ date }}</div>
+        <span>{{ type }}</span>
+      </div>
+      <p>
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titleCard: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  name: "cardPreviewProject",
+};
+</script>
+
+<style>
+.cardPreview {
+  min-height: 13.75rem;
+  margin: 1rem 0;
+}
+.card__info {
+  width: 100%;
+}
+.card__info > h3 {
+  font-size: var(--step-1);
+  margin: 0.5rem 0;
+}
+.card__type {
+  display: flex;
+}
+.card__date {
+  width: 3.875rem;
+  height: 1.5625rem;
+  border-radius: 5rem;
+  background: var(--bg-grey);
+  color: #fff;
+  display: grid;
+  place-items: center;
+}
+.card__type > span {
+  font-size: var(--step--1);
+  color: #8695a4;
+  padding-left: 1.6rem;
+}
+.card__info > p {
+  font-size: var(--step--2);
+  /* justify-self: flex-end; */
+}
+@media screen and (min-width: 48em) {
+  .cardPreview {
+    display: flex;
+    align-items: center;
+  }
+  .card__info {
+    margin-left: 1.5rem;
+  }
+}
+</style>
---
+++ src/modules/website/components/PreviewProjects.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="previewProjects">
+    <h3>Ultimos proyectos</h3>
+    <CardPreviewProject
+      titleCard="Portafolio"
+      date="2021"
+      type="Sitio Web"
+      description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+        laudantium possimus blanditiis rerum dolore ab unde amet numquam."
+    >
+      <template v-slot:img>
+        <img
+          src="../../../assets/img/mia-ecommerce.png"
+          alt="proyecto ecommerce"
+          class="card__img"
+        />
+      </template>
+    </CardPreviewProject>
+    <CardPreviewProject
+      titleCard="Control de Reptiles"
+      date="2021"
+      type="App de control"
+      description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+        laudantium possimus blanditiis rerum dolore ab unde amet numquam."
+    >
+      <template v-slot:img>
+        <img
+          src="../../../assets/img/mia-ecommerce.png"
+          alt="proyecto ecommerce"
+          class="card__img"
+        />
+      </template>
+    </CardPreviewProject>
+    <CardPreviewProject
+      titleCard="Ecommerce React"
+      date="2021"
+      type="Ecommerce"
+      description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+        laudantium possimus blanditiis rerum dolore ab unde amet numquam."
+    >
+      <template v-slot:img>
+        <img
+          src="../../../assets/img/mia-ecommerce.png"
+          alt="proyecto ecommerce"
+          class="card__img"
+        />
+      </template>
+    </CardPreviewProject>
+  </section>
+</template>
+
+<script>
+import CardPreviewProject from "../components/CardPreviewProject";
+export default {
+  name: "previewProjects",
+  components: {
+    CardPreviewProject,
+  },
+};
+</script>
+
+<style>
+.previewProjects {
+  width: 80%;
+}
+</style>
